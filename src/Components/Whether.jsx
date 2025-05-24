0e48521dfd623c383132fec53c68f1f9f1d31d1d
Shahid Ansari <shahidansaribhodu@gmail.com>
2025-05-24 22:56:53 +0530

    changed folder

--- src/Components/Whether.jsx
+++ src/Components/Whether.jsx
@@ -4,8 +4,9 @@ const Whether = () => {
   return (
     <div className=''>
       <div>
-        <input type="text" placeholder='search' />
-        <img src="" alt="" />
+        <input type="text" placeholder='search'
+        className='border-1 rounded-2xl p-5 ' />
+        <img src=".\assets\search.png" alt="" />
       </div>
     </div>
   )
