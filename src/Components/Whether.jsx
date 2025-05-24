b293fdda73832064b2664eb7514169975e0a947e
Shahid Ansari <shahidansaribhodu@gmail.com>
2025-05-25 01:24:15 +0530

    modify ui

--- src/Components/Whether.jsx
+++ src/Components/Whether.jsx
@@ -12,14 +12,22 @@ const Whether = () => {
       <img src="clear.png" alt="" className='w-[150px] place-self-center' />
       <p className='place-self-center font-bold text-2xl text-white'>16 °C </p>
       <p className='place-self-center text-5xl font-bold text-amber-100'>London</p>
-      <div className='flex justify-between mt-5 text-white'>
-      <div>
-        <img src="humidity.png" alt="" />
-        <p className='mt-2'>Humidity</p>
-      </div>
-      <div>
-        <img src="wind.png" alt="" />
-        <p className='mt-2'>Wind</p>
+      <div className='flex justify-between mt-6 text-white'>
+        
+        <div className='flex gap-2'>
+        <img src="humidity.png" alt="" className='w-10 h-10 mt-[5px]'/>
+        <div>
+        <p className='text-[15px]'>36%</p>
+        <p>Humidity</p>
+        </div>
+        </div>
+      
+        <div className='flex gap-2'>
+        <img src="wind.png" alt="" className='w-10 h-10 mt-[5px]'/>
+        <div>
+        <p className='text-[15px]'>32 km/hr</p>
+        <p>Wind</p>
+        </div>
       </div>
     </div>
     </div>
