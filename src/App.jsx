43eb41dc936b467b73e15858edb5ce558c9e5fcf
Shahid Ansari <shahidansaribhodu@gmail.com>
2025-05-25 03:32:26 +0530

    update some designs

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Whether from "./Components/Whether"
 function App() {
 
   return (
-    <div className="app">
+    <div className="app min-h-screen bg-gradient-to-br from-indigo-900 via-purple-800 to-blue-800 flex items-center justify-center">
       <Whether/>
     </div>
   )
--- src/Components/Whether.jsx
+++ src/Components/Whether.jsx
@@ -3,24 +3,23 @@ import React, { useEffect, useRef, useState } from "react";
 const Whether = () => {
   const inputRef = useRef();
   const [data, setdata] = useState(false);
-  
 
-//   const allicon = {
-//     "01d": "clear.png",
-//     "01n": "clear.png",
-//     "02d": "cloud.png",
-//     "02n": "cloud.png",
-//     "03d": "cloud.png",
-//     "03n": "cloud.png",
-//     "04d": "drizzle.png",
-//     "04n": "drizzle.png",
-//     "09d": "rain.png",
-//     "09n": "rain.png",
-//     "10d": "rain.png",
-//     "10n": "rain.png",
-//     "13d": "snow.png",
-//     "13n": "snow.png",
-//   };
+  //   const allicon = {
+  //     "01d": "clear.png",
+  //     "01n": "clear.png",
+  //     "02d": "cloud.png",
+  //     "02n": "cloud.png",
+  //     "03d": "cloud.png",
+  //     "03n": "cloud.png",
+  //     "04d": "drizzle.png",
+  //     "04n": "drizzle.png",
+  //     "09d": "rain.png",
+  //     "09n": "rain.png",
+  //     "10d": "rain.png",
+  //     "10n": "rain.png",
+  //     "13d": "snow.png",
+  //     "13n": "snow.png",
+  //   };
 
   const search = async (city) => {
     if (city === "") {
@@ -34,12 +33,12 @@ const Whether = () => {
 
       const response = await fetch(url);
       const data = await response.json();
-      if(!response.ok){
+      if (!response.ok) {
         alert(data.message);
       }
       console.log(data);
-    //   const icon = allicon[data.weather[0].icon] || "clear.png";
-    const icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      //   const icon = allicon[data.weather[0].icon] || "clear.png";
+      const icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
       setdata({
         humidity: data.main.humidity,
         wind: data.wind.speed,
@@ -48,7 +47,7 @@ const Whether = () => {
         icon: icon,
       });
     } catch (error) {
-        setdata(false);
+      setdata(false);
       console.log(error.message);
     }
   };
@@ -58,7 +57,10 @@ const Whether = () => {
   }, []);
 
   return (
-    <div className="place-self-center p-10 rounded-[20px] bg-gradient-to-r from-indigo-600 to-purple-500 flex flex-col shadow-amber-600 border-4">
+    <div className=
+    "place-self-center p-10 rounded-[20px] bg-white/10 backdrop-blur-md flex flex-col shadow-lg border border-white/30"
+    // "place-self-center p-10 rounded-[20px] bg-gradient-to-r from-indigo-600 to-purple-500 flex flex-col shadow-amber-600 border-4"
+    >
       <div className="place-self-center mb-4 text-2xl font-bold text-white">
         Live Weather
       </div>
@@ -66,21 +68,25 @@ const Whether = () => {
         <input
           ref={inputRef}
           type="text"
-          placeholder="search"
-          className="border rounded-3xl p-2 bg-white text-xl pl-4 pr-4"
+          placeholder="Search city..."
+          className="rounded-full px-6 py-3 text-lg outline-none bg-white/20 text-white placeholder-white/70 border border-white/30"
         />
         <img
           src="search.png"
           alt=""
-          className="w-10 h-11 p-1 "
+          className="px-4 py-3 bg-white/20 rounded-full hover:bg-white/30 transition"
           onClick={() => search(inputRef.current.value)}
         />
       </div>
       {data ? (
         <>
-          <img src={data.icon} alt="" className="w-[150px] place-self-center" />
+          <img
+            src={data.icon}
+            alt=""
+            className="place-self-center w-[140px] drop-shadow-lg animate-pulse"
+          />
           <p className="place-self-center font-bold text-6xl text-white">
-            {data.temperature} °C{" "}
+            {data.temperature} °C
           </p>
           <p className="place-self-center text-4xl font-bold text-amber-100">
             {data.location}
@@ -105,7 +111,9 @@ const Whether = () => {
         </>
       ) : (
         <>
-        <div className="text-2xl mt-3 text-white font-bold">Something went Wrong!</div>
+          <div className="text-2xl mt-3 text-white font-bold">
+            Something went Wrong!
+          </div>
         </>
       )}
     </div>
